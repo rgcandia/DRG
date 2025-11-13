f7ed628622d30bca78a3730da3935f7b0bdd8fc2
Ale Candia <alejandrogcandia@gmail.com>
2025-11-13 09:19:14 -0300

    se crea el boton de whatssap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 
 import Nav from './components/Nav/Nav'
+import styles from './App.module.css'
 function App() {
 
   return (
-    <>
+    <div>
 <Nav/>
-    </>
+
+
+
+
+
+      <a
+        href="https://wa.me/5491156191204"
+        className={styles.wafloat}
+        target="_blank"
+        rel="noopener"
+      >
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+          alt="WhatsApp"
+        />
+      </a>
+
+    </div>
   );
 }
 

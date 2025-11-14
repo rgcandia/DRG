4df4330234ba5fe6cb7866d325ace29c50d5baeb
Ale Candia <alejandrogcandia@gmail.com>
2025-11-14 12:46:10 -0300

    se configura  pre carga de video background

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,38 @@ import Loader from './components/Loader/Loader'; // Importa tu componente de Loa
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [videoLoaded, setVideoLoaded] = useState(false);
 
-  // Simulación de carga de datos (puedes reemplazarla por una lógica real)
+  // Función para precargar el video
+  useEffect(() => {
+    const preloadVideo = () => {
+      const video = document.createElement('video'); // Crea un elemento video
+      video.src = '/video/fondo-pc.mp4'; // Ruta del video a precargar
+      video.preload = 'auto'; // Establece la propiedad preload a 'auto'
+      
+      video.oncanplaythrough = () => {
+        setVideoLoaded(true); // Cuando el video esté completamente cargado, actualiza el estado
+      };
+      
+      video.load(); // Inicia la carga del video
+    };
+
+    preloadVideo();
+  }, []);
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false); // Simula que terminó la carga
-    }, 3000); // Simula una carga de 3 segundos
+      setLoading(false); // Después de la simulación de carga, termina
+    }, 3000); // Simula la carga durante 3 segundos
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div >
-      {loading ? (
-       
-          <Loader /> 
-        
+    <div>
+      {loading || !videoLoaded ? (
+        <Loader /> // Muestra el loader si el video no está cargado o la simulación no ha terminado
       ) : (
-        <Outlet /> // Muestra las rutas cuando ya no esté cargando
+        <Outlet /> // Muestra las rutas cuando ya esté todo cargado
       )}
     </div>
   );
